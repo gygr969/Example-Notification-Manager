51febdfeb79983263042164e4fa80412610f702c
Guille Garcia Rebolo <guillermogarcia_18@hotmail.com>
2016-05-17 11:28:18 +0200

    Update NotificationPublisher.java

--- NotificationPublisher.java
+++ NotificationPublisher.java
@@ -9,8 +9,8 @@ public class NotificationPublisher extends BroadcastReceiver
     public void onReceive(Context context, Intent intent)
     {
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = intent.getParcelableExtra(context.getString("NOTIFICATION"));
-        int id = intent.getIntExtra(context.getString("NOTIFICATION_ID"),0);
+        Notification notification = intent.getParcelableExtra("NOTIFICATION");
+        int id = intent.getIntExtra("NOTIFICATION_ID",0);
         notificationManager.notify(id, notification);
     }
 }
